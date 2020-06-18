b42e50bce0f50e995453a9bf24e0878725ce74fc
ntDat <dat.nt2212@sinhvien.hoasen.edu.vn>
2020-06-18 11:35:24 +0700

    mern-v2

---
+++ mern-v2/backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: "mongodb+srv://admin:123@cluster0-cr5lz.gcp.mongodb.net/MERN?retryWrites=true&w=majority"
+};
---
+++ mern-v2/backend/models/student.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let studentSchema = new Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  rollno: {
+    type: Number
+  }
+}, {
+    collection: 'Students'
+  })
+
+module.exports = mongoose.model('Student', studentSchema)
